f62b67bd6063aeedffd988fb2b410e37b0d60b32
samokhin123 <samokhina29@yandex.ru>
2025-09-04 18:46:55 +0300

    homework

--- index.py
+++ index.py
@@ -1,2 +1,18 @@
-a="seal"
-print(a)
+text="Программирование - это процесс создания программ. Программирование помогает автоматизировать работу. Программирование интересно и полезно!"
+text=text.split(" ")
+text.remove("-")
+text2=set(text)
+print(len(text2))
+c=0
+b=0
+for i in text2:
+    a= len(i)
+    if a > c:
+        c=a
+        b=i
+print("Самое длинное слово:", b, "длина слова:", c)
+def len_word (d):
+    g=len(d)
+    return g
+len_word=len_word(str(input()))
+print("длина слова:", len_word)
